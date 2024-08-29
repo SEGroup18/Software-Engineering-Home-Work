7d1df1ea69bb64cc5a48d4780a06b0ed78133044
Aniruddha Shivananda <aniruddhams2023@gmail.com>
2024-08-29 14:03:06 +0000

    Add a python Program to myFile

---
+++ myfile.py
@@ -0,0 +1,18 @@
+def compute_perimeter(radius):
+    #perimeter formulae = 2 * π * radius
+    perimeter = 2 * 3.14 * radius
+    return perimeter
+
+def print_info_of_circle(radius):
+    perimeter = compute_perimeter(radius)
+    print("Radius is : ", radius)
+    print("Perimeter is:", perimeter)
+
+def main():
+    radius = 5
+    print_info_of_circle(radius)
+    # undefined variable 'diameter' to showcase error
+    print("Diameter is:", 2 * radius)
+
+if __name__ == "__main__":
+    main()
